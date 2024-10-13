9a5eb27dd71439ccb4eb53c174a150a72fa8de76
Liam Robinson <robin502@purdue.edu>
2024-10-13 13:57:29 -0400

    removed ssh tunnel

---
+++ examples/percent_submissions_to_answer.py
@@ -0,0 +1,10 @@
+import polars as pl
+
+import src
+
+
+df = src.load_tables('submission')
+df = df.with_columns(
+    not_attempted=pl.col('userSolution').list.len() == 0
+)
+print(df['not_attempted'].sum() / df.height * 100)
